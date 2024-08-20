91fd31a81e173af67ce24351d0680751e28fe57f
Tino <tino.stoeckli@icloud.com>
2024-08-20 18:05:07 +0200

    Quciksave

--- food_tracker_console_app/Program.cs
+++ food_tracker_console_app/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/*INSERT INTO [Product] (EANCode, ProductName, Energy, Fat, Carbohydrate, Sugar, Protein)
+VALUES (153990100000, 'Kichererbsen', 337, 5.9, 44, 2.4, 19)*/
+//https://stackoverflow.com/questions/64946459/uploading-a-database-to-github
